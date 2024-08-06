b3d10a10ae7fe19fefd07b5da1957e746c80d64e
VKTRenokh <viktorenokh@gmail.com>
2024-08-06 11:12:41 +0300

    feat: sentinal array

--- zig/cmup/src/hello.zig
+++ zig/cmup/src/hello.zig
@@ -20,4 +20,20 @@ pub fn main() void {
     ptr.* = 101;
 
     std.debug.print("x: {}\n", .{x});
+
+    const @"Funny variable" = 50;
+
+    std.debug.print("{}\n", .{@"Funny variable"});
+
+    const a = [_:255]i32{
+        1,
+        2,
+        4,
+        0,
+        1,
+        2,
+    };
+
+    std.debug.print("{}\n", .{a[6]}); // should print 255
+    std.debug.print("{any}\n", .{a});
 }
