b49627b98071a6c78b3e56befa8d1eecfe8c443d
VKTRenokh <viktorenokh@gmail.com>
2024-08-14 09:10:38 +0300

    feat: add playlist output

--- zig/cmup/src/main.zig
+++ zig/cmup/src/main.zig
@@ -1,25 +1,36 @@
 const std = @import("std");
 
-const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const cmus_path = "/home/vktrenokh/music";
 
-pub fn getCmupThings(allocator: std.mem.Allocator) anyerror!std.ArrayListAligned([]const u8, null) {
-    var dir = try std.fs.openDirAbsolute("/", .{ .iterate = true });
+const CmupPlaylist = struct {
+    name: []const u8,
+    content: []const u8,
+    path: []const u8,
+};
+
+pub fn getCmupThings(allocator: std.mem.Allocator) anyerror!std.ArrayListAligned(CmupPlaylist, null) {
+    var dir = try std.fs.openDirAbsolute(cmus_path, .{ .iterate = true });
     defer dir.close();
 
     var iter = dir.iterate();
 
-    var result = std.ArrayList([]const u8).init(allocator);
+    var result = std.ArrayList(CmupPlaylist).init(allocator);
 
     while (try iter.next()) |value| {
-        try result.append(value.name);
+        const playlist = CmupPlaylist{ .name = value.name, .path = try std.fs.path.join(allocator, &.{ cmus_path, value.name }), .content = "" };
+        try result.append(playlist);
     }
 
     return result;
 }
 
-pub fn printArrayOfStrings(str_array: [][]const u8) void {
-    for (str_array) |item| {
-        std.debug.print("{s}\n", .{item});
+pub fn printCmupPlaylist(playlist: CmupPlaylist) void {
+    std.debug.print("Playlist {s}, on path {s}, with music amount {}\n", .{ playlist.name, playlist.path, playlist.content.len });
+}
+
+pub fn printCmupPlaylists(playlists: []const CmupPlaylist) void {
+    for (playlists) |item| {
+        printCmupPlaylist(item);
     }
 }
 
@@ -30,5 +41,5 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     const cmup = try getCmupThings(allocator);
-    printArrayOfStrings(cmup.items);
+    printCmupPlaylists(cmup.items);
 }
