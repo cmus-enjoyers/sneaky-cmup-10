b3020518ed7c3ee9d718a6d32902cb5ed198fe63
VKTRenokh <viktorenokh@gmail.com>
2024-08-20 11:18:14 +0300

    feat: add directory listing

--- emacs-lisp/cmup/src/main.el
+++ emacs-lisp/cmup/src/main.el
@@ -1,1 +1,11 @@
-(print '(rose violet daisy cmup))
+(require 'cl-lib)
+
+(defconst cmup-music-path 
+          (expand-file-name "~/music"))
+
+(defun get-cmup-music ()
+ (cl-remove-if (lambda (str)
+                 (string-prefix-p "." str))
+               (directory-files cmup-music-path)))
+
+(print (get-cmup-music))
