f596c5d5b0ec1ea3e07f040acdce39092d6ec5f0
VKTRenokh <viktorenokh@gmail.com>
2024-08-21 10:19:41 +0300

    feat: add cmup-playlist struct

--- emacs-lisp/cmup/src/main.el
+++ emacs-lisp/cmup/src/main.el
@@ -1,11 +1,31 @@
 (require 'cl-lib)
 
-(defconst cmup-music-path 
-          (expand-file-name "~/music"))
+(cl-defstruct cmup-playlist
+  name
+  path
+  content)
+
+(defconst cmup-music-path
+  (expand-file-name "~/music"))
+
+(defconst cmup-playlist-path
+  (expand-file-name "~/.config/cmus/playlists"))
+
+(defun ls (path)
+  (cl-remove-if (lambda (str)
+                  (string-prefix-p "." str))
+                (directory-files path)))
+
+(defun make-playlist-path (path)
+  (concat cmup-playlist-path "/" path))
+
+(defun create-playlist (name)
+  (let ((path (make-playlist-path name)))
+    (make-cmup-playlist :name name :path path :content nil)))
 
 (defun get-cmup-music ()
- (cl-remove-if (lambda (str)
-                 (string-prefix-p "." str))
-               (directory-files cmup-music-path)))
+ (ls cmup-music-path))
 
-(print (get-cmup-music))
+(print (mapcar (lambda (x)
+          (create-playlist x))
+        (get-cmup-music)))
