83a8525acb0fedf9dfcfef7e4604da90201a6fe8
VKTRenokh <viktorenokh@gmail.com>
2024-08-21 10:47:41 +0300

    feat: add music listing

--- emacs-lisp/cmup/src/main.el
+++ emacs-lisp/cmup/src/main.el
@@ -11,17 +11,32 @@
 (defconst cmup-playlist-path
   (expand-file-name "~/.config/cmus/playlists"))
 
-(defun ls (path)
+(defun ls (path &optional filter-fn)
+  "List non-hidden files in path. Optionally filter with filter-fn"
   (cl-remove-if (lambda (str)
-                  (string-prefix-p "." str))
+                  (or (string-prefix-p "." str)
+                      (and filter-fn
+                           (funcall filter-fn str))))
                 (directory-files path)))
 
 (defun make-playlist-path (path)
   (concat cmup-playlist-path "/" path))
 
+(defun make-music-path (path)
+  (concat cmup-music-path "/" path))
+
+(defun music? (name)
+  (string-suffix-p ".mp3" name))
+
 (defun create-playlist (name)
-  (let ((path (make-playlist-path name)))
-    (make-cmup-playlist :name name :path path :content nil)))
+  (let ((path (make-playlist-path name))
+        (music-path (make-music-path name)))
+    (make-cmup-playlist :name name :path path :content
+                                      (ls music-path (lambda (str)
+                                        (not (music? str)))))))
+
+(defun write-playlist (playlist)
+  nil)
 
 (defun get-cmup-music ()
  (ls cmup-music-path))
