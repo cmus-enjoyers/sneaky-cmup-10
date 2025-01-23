2f96c1013bf4f9ef4202a82e102501abfd6930bb
VKTRenokh <viktorenokh@gmail.com>
2025-01-23 17:06:08 +0200

    feat: add some kind of specification

--- zig/cmup/src/zql.zig
+++ zig/cmup/src/zql.zig
@@ -1,3 +1,22 @@
+// Example of a ZQL query:
+//
+// require playlists jump-bangers, vktrenokh-stwv
+//
+// add all from jump-bangers and add all from vktrenokh-stwv where name contains 'voj'
+//
+
+// The name of the new playlist should be derived from the ZQL file name
+// (e.g., "vktrenokh-eurobeat.zql" => "vktrenokh-eurobeat").
+//
+// The `require` statement specifies which playlists will be used to create the new playlist.
+// - If a playlist is referenced later in the query but is not defined using `require`,
+//   the query should terminate with an error.
+// - If a playlist is defined in the `require` statement but is not found by the `zmup` program,
+//   the query should also terminate with an error.
+//
+// String literals should only be created using single quotes ('').
+// The single quote character (') within a string can be escaped using the backslash (\) character.
+
 const std = @import("std");
 
 pub const TokenType = enum {
