b54ee8a402d9dece0a8a1a9762a993d15138d673
VKTRenokh <viktorenokh@gmail.com>
2025-01-30 17:31:06 +0200

    fix: bug with `where name contains home`

--- zig/cmup/src/zql/filters/filter-by-name.zig
+++ zig/cmup/src/zql/filters/filter-by-name.zig
@@ -5,15 +5,15 @@ const path = @import("../../utils/path.zig");
 
 pub fn filterByName(result: *std.ArrayList([]const u8), playlist: CmupPlaylist, filter: Filter) !void {
     for (playlist.content) |track| {
+        const name = path.getFileNameWithoutExtension(track);
+
         switch (filter.match_type) {
             .Contains => {
-                if (std.ascii.indexOfIgnoreCase(track, filter.target) != null) {
+                if (std.ascii.indexOfIgnoreCase(name, filter.target) != null) {
                     try result.append(track);
                 }
             },
             .Is => {
-                const name = path.getFileNameWithoutExtension(track);
-
                 if (std.mem.eql(u8, name, filter.target)) {
                     try result.append(track);
                 }
